01385173b20bb32d92311a8a0174faf238af0793
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-11-23 20:17:51 -0300

    script de limpeza de tces

---
+++ src/cleaners/clean_tces.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+def ler_dados(url: str) -> pd.DataFrame:
+    convenios_dataframe = pd.read_parquet(url)
+
+    return convenios_dataframe
+
+def remover_colunas(database: pd.DataFrame) -> pd.DataFrame:
+    colunas_para_remocao = [
+    "Tema provável", "Nº do Processo da Unidade Responsável pelo Recurso", "Nº Original do Instrumento de Repasse",
+    "Nº SIAFI do Instrumento de Repasse", "Nº Siconvi", "Unidade", "Valor Original", "(c) Valor Corrigido a Ser Restituído aos Cofres Públicos (R$)",
+    "Função Programática", "code_muni", "name_muni", "code_state", "code_region", "percent_ganhador", "Ideologia",
+    "percent_ganhador_1996", "PERCENT_ganhador_2000", "PERCENT_ganhador_2004", "PERCENT_ganhador_2008", "PERCENT_ganhador_2012", "PERCENT_ganhador_médio",
+    "Ideologia1996", "Ideologia2000", "Ideologia2004", "Ideologia2008", "ideologia 2012", "Partido 1997", "Partido_2000", "Partido_2004",
+    "Partido_2008", "Partido_2012", "ideologia_media", "IDHM_2000", "IDHM_2010", "IDHM_media", "tm 2000", "tm 2004", "tm 2008", "tm2012", "tamanho setor media",
+    "NEP1996", "NEP2000", "NEP2004", "NEP2008", "NEP2012", "NEP_medio",
+    "PIBpercapita1996","Pibpercapita2000", "Pibpercapita2004", "Pibpercapita2008", "Pibpercapita2012", "Pibpercapita_medio", 
+    "TaxadeUrbanização1996", "TaxadeUrbanização2000", "TaxadeUrbanização2004", "TaxadeUrbanização2008", "TaxadeUrbanização_media", 
+    "Arrec_Prop2000", "Arrec_Prop2004", "Arrec_Prop2008", "Arrec_Prop2012", "Arrec_Prop_media", "POP", "LN_POP"
+]
+
+    database.drop(columns=colunas_para_remocao, inplace=True)
+
+    return database
+
+def formatar_nome_colunas(database: pd.DataFrame) -> pd.DataFrame:
+    nomes_formatados = {
+    'Ano da instauração do processo pela CGU': 'ano_instauracao_tce',
+    'UF.x': 'unidade_federativa',
+    'name_region': 'regiao',
+    'Ministério': 'ministerio',
+    'ano_provavel_contrato': 'ano_contrato',
+    'Motivo da Instauração da TCE': 'motivo_instauracao_tce',
+    }
+
+    database.rename(columns=nomes_formatados, inplace=True)
+
+    return database
+
+def padronizar_tipos(database: pd.DataFrame) -> pd.DataFrame:
+    
+    database['ministerio'] = database['ministerio'].astype('category')
+    database['regiao'] = database['regiao'].astype('category')
+    database['unidade_federativa'] = database['unidade_federativa'].astype('category')
+    database['motivo_instauracao_tce'] = database['motivo_instauracao_tce'].astype('category')
+    database['ano_contrato'] = database['ano_contrato'].astype('Int16')
+    database['ano_instauracao_tce'] = database['ano_instauracao_tce'].astype('Int16')
+
+    return database
+    
+
+def limpar_database_convenios(url) -> pd.DataFrame:
+    
+    df_tces = ler_dados(url)
+    df_tces = remover_colunas(df_tces)
+    df_tces = formatar_nome_colunas(df_tces)
+    df_tces = padronizar_tipos(df_tces)
+
+    return df_tces
+
+if __name__ == "__main__":
+    convenios_database_url = './database/raw/tces.parquet'
+    local_salvamento = './database/clean/tces_clean.parquet'
+
+    convenios_limpo = limpar_database_convenios(convenios_database_url)    
+    convenios_limpo.to_parquet(local_salvamento, index=False)
+
+    print(f"Arquivo limpo salvo!")
