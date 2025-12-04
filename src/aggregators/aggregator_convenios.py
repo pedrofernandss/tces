dddfcb97df873c278380967ed88b8f81f3f74c76
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-04 15:12:06 -0300

    deixando o código mais pytonico

--- src/aggregators/aggregator_convenios.py
+++ src/aggregators/aggregator_convenios.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
+
 def ler_dados(url: str) -> pd.DataFrame:
     tces_dataframe = pd.read_parquet(url)
 
     return tces_dataframe
 
+
 def contar_convenios_por_ministerio(dataframe: pd.DataFrame) -> pd.DataFrame:
     contagem_convenios_por_ministerio = dataframe.groupby(
-    ['ministerio', 'ano_referencia']).size().reset_index(name='quantidade_convenios')
+        ['ministerio', 'ano_referencia']).size().to_frame(name='quantidade_convenios')
 
     return contagem_convenios_por_ministerio
+
 
 def contar_convenios_por_regiao(dataframe: pd.DataFrame) -> pd.DataFrame:
 
@@ -25,49 +28,53 @@ def contar_convenios_por_regiao(dataframe: pd.DataFrame) -> pd.DataFrame:
 
     convenios_pivot_regiao = pd.crosstab(
         index=[dataframe['ministerio'],
-        dataframe['ano_referencia']], 
+               dataframe['ano_referencia']],
         columns=dataframe['regiao']
     )
 
-    convenios_pivot_regiao.columns = [f'qntd_convenios_{r.lower().replace("-", "_")}' for r in convenios_pivot_regiao.columns]
+    convenios_pivot_regiao.columns = [
+        f'qntd_convenios_{r.lower().replace("-", "_")}' for r in convenios_pivot_regiao.columns]
 
     return convenios_pivot_regiao
 
+
 def contar_convenios_por_situacao(dataframe: pd.DataFrame) -> pd.DataFrame:
-        
+
     convenios_pivot_situacao = pd.crosstab(
         index=[dataframe['ministerio'], dataframe['ano_referencia']],
         columns=dataframe['situacao_convenio']
     )
 
     convenios_pivot_situacao.columns = [f'qtnd_convenios_{m.lower().replace("Ã", "A").replace("Ç", "C").replace(" ", "_")}'
-        for m in convenios_pivot_situacao.columns
-    ]
+                                        for m in convenios_pivot_situacao.columns
+                                        ]
 
     return convenios_pivot_situacao
+
 
 def contar_convenios_por_alinhamento(dataframe: pd.DataFrame) -> pd.DataFrame:
 
     convenios_pivot_alinhamento = pd.crosstab(
-        index=[dataframe['ministerio'],dataframe['ano_referencia']],
+        index=[dataframe['ministerio'], dataframe['ano_referencia']],
         columns=dataframe['alinhamento_gov']
     )
 
     mapa_alinhamento = {0: 'nao_alinhado', 1: 'alinhado'}
-    convenios_pivot_alinhamento.columns = [f'qntd_convenios_{mapa_alinhamento.get(col, col)}' for col in convenios_pivot_alinhamento.columns]
+    convenios_pivot_alinhamento.columns = [
+        f'qntd_convenios_{mapa_alinhamento.get(col, col)}' for col in convenios_pivot_alinhamento.columns]
 
     return convenios_pivot_alinhamento
 
-def somar_valor_convenios(dataframe: pd.DataFrame) -> pd.DataFrame: 
-    
-    df_valor_convenios_somado = dataframe.groupby(['ministerio', 'ano_referencia'])[
-        'valor_convenio_deflacionado'].sum().reset_index(name='valor_total_convenios')
 
-    
-    df_valor_convenios_somado['valor_total_convenios'] = df_valor_convenios_somado['valor_total_convenios'].round(
-        2)
-    
+def somar_valor_convenios(dataframe: pd.DataFrame) -> pd.DataFrame:
+
+    df_valor_convenios_somado = dataframe.groupby(['ministerio', 'ano_referencia'])[
+        'valor_convenio_deflacionado'].sum().to_frame(name='valor_total_convenios')
+
+    df_valor_convenios_somado['valor_total_convenios'] = df_valor_convenios_somado['valor_total_convenios']
+
     return df_valor_convenios_somado
+
 
 def agregar_base_convenios(url: str) -> pd.DataFrame:
     df_convenios = ler_dados(url)
@@ -78,10 +85,12 @@ def agregar_base_convenios(url: str) -> pd.DataFrame:
     df_conv_agg_situacao = contar_convenios_por_situacao(df_convenios)
     df_conv_agg_alinhamento = contar_convenios_por_alinhamento(df_convenios)
 
-    tces_final = pd.concat([df_conv_agg_ministerios, df_conv_agg_situacao, df_conv_agg_regiao, df_conv_agg_alinhamento, df_conv_valores_somado], axis=1)
+    tces_final = pd.concat([df_conv_agg_ministerios, df_conv_agg_situacao,
+                           df_conv_agg_regiao, df_conv_agg_alinhamento, df_conv_valores_somado], axis=1)
     tces_final = tces_final.reset_index()
 
     return tces_final
+
 
 if __name__ == "__main__":
     tces_database_url = './database/clean/convenios_clean.parquet'
