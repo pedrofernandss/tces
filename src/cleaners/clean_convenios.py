51d7394672a693108f77d23c1df06885e4ed84e5
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-07 21:06:45 -0300

    padronização do nome dos ministérios

--- src/cleaners/clean_convenios.py
+++ src/cleaners/clean_convenios.py
@@ -51,18 +51,62 @@ def remover_colunas(database: pd.DataFrame) -> pd.DataFrame:
 def padronizar_tipos(database: pd.DataFrame) -> pd.DataFrame:
 
     database['numero_convenio'] = database['numero_convenio'].astype("Int32")
-    database['ministerio'] = database['ministerio'].astype(
-        str).str.title().str.strip()
+    database['ministerio'] = database['ministerio'].astype(str).str.title().str.strip()
     database['ministerio'] = database['ministerio'].astype('category')
     database['ano_referencia'] = database['ano_referencia'].astype("Int64")
-    database['situacao_convenio'] = database['situacao_convenio'].astype(
-        'category')
-    database['tipo_instrumento'] = database['tipo_instrumento'].astype(
-        'category')
-    database['alinhamento_gov'] = database['alinhamento_gov'].astype(
-        pd.Int8Dtype())
-    database['alinhamento_final'] = database['alinhamento_final'].astype(
-        pd.Int8Dtype())
+    database['situacao_convenio'] = database['situacao_convenio'].astype('category')
+    database['tipo_instrumento'] = database['tipo_instrumento'].astype('category')
+    database['alinhamento_gov'] = database['alinhamento_gov'].astype(pd.Int8Dtype())
+    database['alinhamento_final'] = database['alinhamento_final'].astype(pd.Int8Dtype())
+
+    return database
+
+def padronizar_nome_ministerios(database: pd.DataFrame) -> pd.DataFrame:
+
+    relacao_nomes_ministerio = {
+        "Ministério Da Agricultura, Pecuária E Abastecimento": "Ministério da Agricultura e Pecuária",
+        "Ministério Da Agricultura E Pecuária": "Ministério da Agricultura e Pecuária",
+        "Ministério Da Agricultura, Da Pecuária E Do Abastecimento": "Ministério da Agricultura e Pecuária",
+        "Ministério Da Ciência E Tecnologia": "Ministério da Ciência e Tecnologia",
+        "Ministério Da Ciência, Tecnologia E Inovação": "Ministério da Ciência e Tecnologia",
+        "Ministério Da Ciência, Tecnologia, Inovações E Comunicações": "Ministério da Ciência e Tecnologia",
+        "Ministério Da Ciência, Tec, Inov. E Com.": "Ministério da Ciência e Tecnologia",
+        "Ministério Da Integração E Do Desenvolvimento Regional": "Ministério Da Integração Nacional",
+        "Ministério Da Indústria, Comércio Exterior E Serviços": "Ministério Da Indústria, Comércio Exterior E Serviços",
+        "Ministério Do Desenvolvimento, Indústria, Comércio E Serviços": "Ministério Da Indústria, Comércio Exterior E Serviços",
+        "Ministério Do Desenvolvimento, Indústria E Comércio Exterior": "Ministério Da Indústria, Comércio Exterior E Serviços",
+        "Ministério Das Mulheres": "Ministério Das Mulheres, Igualdade Racial, Da Juventude E Dos Direitos Humanos",
+        "Ministério Dos Direitos Humanos E Cidadania": "Ministério Das Mulheres, Igualdade Racial, Da Juventude E Dos Direitos Humanos",
+        "Ministério Dos Direitos Humanos": "Ministério Das Mulheres, Igualdade Racial, Da Juventude E Dos Direitos Humanos",
+        "Ministério Da Igualdade Racial": "Ministério Das Mulheres, Igualdade Racial, Da Juventude E Dos Direitos Humanos",
+        "Ministério Da Justiça E Segurança Pública": "Ministério Da Justiça",
+        "Ministério Da Justiça E Cidadania": "Ministério Da Justiça",
+        "Ministério Da Previdência E Assistência Social": "Ministério Da Previdência Social",
+        "Ministério De Minas E Energia": "Ministério Das Minas E Energia",
+        "Ministério Do Desenvolvimernto Agrário": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento Agrário": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento Social E Agrário": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento Social E Combate À Fome": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento Social E Combate A Fome": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento Agrário E Agricultura Familiar": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Desenvolvimento E Assistência Social, Família E Combate À Fome": "Ministério Do Desenvolvimento Social E Agrário",
+        "Ministério Do Turismo": "Ministério Do Esporte E Turismo",
+        "Ministério Do Esporte": "Ministério Do Esporte E Turismo",
+        "Ministério Do Transportes": "Ministério Dos Transportes",
+        "Ministério Do Trabalho": "Ministério Do Trabalho E Emprego",
+        "Ministério Do Meio Ambiente E Mudança Do Clima": "Ministério Do Meio Ambiente",
+        "Ministério Do Planejamento E Orçamento": "Ministério Do Planejamento, Orçamento E Gestão",
+        "Ministério Da Gestão E Da Inovação Em Serviços Públicos": "Ministério Do Planejamento, Orçamento E Gestão",
+        "Ministério Do Planejamento E Orçamento E Gestão": "Ministério Do Planejamento, Orçamento E Gestão",          
+    }
+
+    database['ministerio'] = database['ministerio'].str.replace(r'^.*[-/]\s*', '', regex=True) # Remoção dos acrónimos e o traço/barra   
+    database['ministerio'] = database['ministerio'].str.strip() # Remoção de espaços extras no início/fim e normalizar
+    database['ministerio'] = database['ministerio'].str.replace(r'Ministérioda', 'Ministério da', regex=True)
+    database['ministerio'] = database['ministerio'].str.title() # Converção de tudo para a primeira letra maiúscula e o resto minúsculo
+    database['ministerio'] = database['ministerio'].replace(relacao_nomes_ministerio)
+    
+    database = database[database['ministerio'] != "Sem Informação"]
 
     return database
 
@@ -73,6 +117,7 @@ def limpar_database_convenios(url) -> pd.DataFrame:
     df_convenios = formatar_nome_colunas(df_convenios)
     df_convenios = extrair_ano(df_convenios)
     df_convenios = remover_colunas(df_convenios)
+    df_convenios = padronizar_nome_ministerios(df_convenios)
     df_convenios = padronizar_tipos(df_convenios)
 
     return df_convenios
