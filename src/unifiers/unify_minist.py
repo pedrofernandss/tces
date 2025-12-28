13d1701030d6752c843b3d3e1b5591425072f214
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-12-28 00:21:24 -0300

    script de unificação de base de dados

---
+++ src/unifiers/unify_minist.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+url = '../database/aggregated/tces_aggregated_minist.parquet'
+tces_dataframe = pd.read_parquet(url)
+
+url = '../database/aggregated/convenios_aggregated_minist.parquet'
+convenios_dataframe = pd.read_parquet(url)
+
+convenios_dataframe['ano_referencia'] = convenios_dataframe['ano_referencia'].astype('Int64')
+tces_dataframe['ano_referencia'] = tces_dataframe['ano_referencia'].astype('Int64')
+
+convenios_dataframe['ministerio'] = convenios_dataframe['ministerio'].astype(str)
+tces_dataframe['ministerio'] = tces_dataframe['ministerio'].astype(str)
+
+dataframe_unificado = pd.merge(convenios_dataframe, tces_dataframe, on=[
+                               'ministerio', 'ano_referencia', 'partido_ano_referencia', 'media_distancia_ideologica'], how='outer')
+
+dataframe_unificado.to_excel('./bases_unificadas_relacao_gov_federal.xlsx', index=False)
