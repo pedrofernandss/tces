7e93b8fe3c312bed2406aed6f649736afc7c2783
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-08 23:38:53 -0300

    script para tces de ministerios especificos

---
+++ src/cleaners/clean_tces_specific_ministerios.py
@@ -0,0 +1,139 @@
+import pandas as pd
+
+
+def ler_dados(url: str) -> pd.DataFrame:
+    convenios_dataframe = pd.read_parquet(url)
+
+    return convenios_dataframe
+
+
+def remover_colunas(database: pd.DataFrame) -> pd.DataFrame:
+    colunas_para_remocao = [
+        "Tema provável", "Nº do Processo da Unidade Responsável pelo Recurso", "Nº Original do Instrumento de Repasse",
+        "Nº SIAFI do Instrumento de Repasse", "Unidade Gestora", "Nº Siconvi", "Unidade", "Valor Original",
+        "(c) Valor Corrigido a Ser Restituído aos Cofres Públicos (R$)",
+        "code_muni", "name_muni", "code_state", "code_region", "name_state",
+        "ideologia_2000", "ideologia_2004", "ideologia_2008", "ideologia_2012", "Partido 1997", "alinhamento.min.pref",
+        "Partido_2000", "Partido_2004", "Partido_2008", "Partido_2012", "alinhamento_tce"
+    ]
+
+    database.drop(columns=colunas_para_remocao, inplace=True, errors='ignore')
+
+    return database
+
+
+def formatar_nome_colunas(database: pd.DataFrame) -> pd.DataFrame:
+    nomes_formatados = {
+        'Ano da instauração do processo pela CGU': 'ano_referencia',
+        'UF.x': 'unidade_federativa',
+        'name_region': 'regiao',
+        'Ministério': 'ministerio',
+        'Motivo da Instauração da TCE': 'motivo_instauracao_tce',
+        'alinhamento_tce1': 'alinhamento_municipio_gov_tce',
+        'alinhamento.min.pref.1': 'alinhamento_municipio_minist_tce',
+        'dista.ideo.min.pref': 'distan_ideologia_municipio_minist',
+        'dista.ideo.gov.pref': 'distan_ideologia_municipio_gov_federal',
+        'governo_tce': 'governo_instaurador_tce'
+    }
+
+    database.rename(columns=nomes_formatados, inplace=True)
+
+    return database
+
+
+def padronizar_tipos(database: pd.DataFrame) -> pd.DataFrame:
+
+    cols_to_clean = ['distan_ideologia_municipio_minist',
+                     'distan_ideologia_municipio_gov_federal']
+    for col in cols_to_clean:
+        if col in database.columns:
+            if database[col].dtype == 'object' or database[col].dtype.name == 'string':
+                database[col] = database[col].astype(str).str.replace(
+                    '.', '', regex=False).str.replace(',', '.', regex=False)
+
+    database['ministerio'] = database['ministerio'].astype(
+        str).str.title().str.strip()
+    database['ministerio'] = database['ministerio'].astype('category')
+    database['ano_referencia'] = database['ano_referencia'].astype('Int32')
+    database['regiao'] = database['regiao'].astype('category')
+    database['unidade_federativa'] = database['unidade_federativa'].astype(
+        'category')
+    database['motivo_instauracao_tce'] = database['motivo_instauracao_tce'].astype(
+        'category')
+    database['distan_ideologia_municipio_minist'] = pd.to_numeric(
+        database['distan_ideologia_municipio_minist'], errors='coerce')
+    database['distan_ideologia_municipio_gov_federal'] = pd.to_numeric(
+        database['distan_ideologia_municipio_gov_federal'], errors='coerce')
+
+    return database
+
+def adicionar_partido_ano_referencia(database: pd.DataFrame) -> pd.DataFrame:
+    def get_partido(row):
+        ano = row['ano_referencia']
+        if 2001 <= ano <= 2004:
+            return row['Partido_2000']
+        elif 2005 <= ano <= 2008:
+            return row['Partido_2004']
+        elif 2009 <= ano <= 2012:
+            return row['Partido_2008']
+        elif 2013 <= ano <= 2016:
+            return row['Partido_2012']
+        return None
+
+    database['partido_ano_referencia'] = database.apply(get_partido, axis=1)
+    return database
+
+
+def padronizar_nome_ministerios(database: pd.DataFrame) -> pd.DataFrame:
+
+    relacao_nomes_ministerio = {
+        "Ministério Do Desenvolvimernto Agrário": "Ministério Do Desenvolvimento Agrário",
+        "Ministério Do Desenvolvimento Social E Combate À Fome": "Ministério Do Desenvolvimento Social E Combate A Fome",
+        "Ministério Do Desenvolvimento Agrário e Agricultura Familiar": "Ministério Do Desenvolvimento Agrário E Agricultura Familiar",
+        "Ministério Do Desenvolvimento e Assistência Social, Família E Combate À Fome": "Ministério Do Desenvolvimento E Assistência Social, Família E Combate À Fome",
+    }
+
+    database['ministerio'] = database['ministerio'].str.replace(
+        r'^.*[-/]\s*', '', regex=True)  # Remoção dos acrónimos e o traço/barra
+    # Remoção de espaços extras no início/fim e normalizar
+    database['ministerio'] = database['ministerio'].str.strip()
+    database['ministerio'] = database['ministerio'].str.replace(
+        r'Ministérioda', 'Ministério da', regex=True)
+    # Converção de tudo para a primeira letra maiúscula e o resto minúsculo
+    database['ministerio'] = database['ministerio'].str.title()
+    database['ministerio'] = database['ministerio'].replace(
+        relacao_nomes_ministerio)
+
+    return database
+
+def remover_ministerios(database: pd.DataFrame) -> pd.DataFrame:
+    ministerios_a_manter = ["Ministério Do Desenvolvimento Agrário", "Ministério Do Desenvolvimento Social E Combate A Fome",
+        "Ministério Do Desenvolvimento Agrário E Agricultura Familiar",
+        "Ministério Do Desenvolvimento E Assistência Social, Família E Combate À Fome",
+        "Ministério Do Turismo",
+        "Ministério Do Esporte"
+    ]
+
+
+
+def limpar_database_convenios(url) -> pd.DataFrame:
+
+    df_tces = ler_dados(url)
+    df_tces = formatar_nome_colunas(df_tces)
+    df_tces = padronizar_tipos(df_tces)
+    df_tces = adicionar_partido_ano_referencia(df_tces)
+    df_tces = remover_colunas(df_tces)
+    df_tces = padronizar_nome_ministerios(df_tces)
+    df_tces = remover_ministerios(df_tces)
+
+    return df_tces
+
+
+if __name__ == "__main__":
+    convenios_database_url = './database/raw/tces.parquet'
+    local_salvamento = './database/clean/tces_clean.parquet'
+
+    convenios_limpo = limpar_database_convenios(convenios_database_url)
+    convenios_limpo.to_parquet(local_salvamento, index=False)
+
+    print(f"Arquivo limpo salvo!")
